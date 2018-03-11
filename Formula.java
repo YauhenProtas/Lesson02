1cd0924fb1d66df734444069b6a1ec3214342919
YauhenProtas <36918779+YauhenProtas@users.noreply.github.com>
2018-03-11 12:18:14 +0300

    Add files via upload

---
+++ Formula.java
@@ -0,0 +1,32 @@
+package by.minsk.jb24;
+
+import java.util.Scanner;
+
+public class Formula {
+
+	public static void main(String[] args) {
+		double a = 0, b = 0, c = 0, result;
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Введите a: ");
+		if (sc.hasNextDouble()) {
+			a = sc.nextDouble();
+		}
+		System.out.print("Введите b: ");
+		if (sc.hasNextDouble()) {
+			b = sc.nextDouble();
+		}
+		System.out.print("Введите c: ");
+		if (sc.hasNextDouble()) {
+			c = sc.nextDouble();
+		}
+
+		// result = a+b+c+d;
+		result = (b + (Math.sqrt(b * b + 4 * a * c))) / 2 * a - a * a * a * c + b;
+
+		System.out.println("Ответ = " + result);
+
+	}
+
+}
