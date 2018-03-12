f9a217f5f6160766272fd25f908cb6834306e29f
YauhenProtas <36918779+YauhenProtas@users.noreply.github.com>
2018-03-12 15:41:08 +0300

    Add files via upload

---
+++ src/third/SumAndMul.java
@@ -0,0 +1,17 @@
+package third;
+
+public class SumAndMul {
+	public static void main(String[] args) {
+		int x;
+		int y;
+		int sum, mul;
+
+		x = 20;
+		y = 30;
+
+		sum = x + y;
+		mul = x * y;
+
+		System.out.println("sum = " + sum + "; mul = " + mul);
+	}
+}
